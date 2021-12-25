28a0b5b3109e061adeba74d1fb4205143858cb19
carlos3g <52337966+carlos3g@users.noreply.github.com>
2021-12-25 20:14:58 -0300

    [FEAT] sets up custom fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 import Navigation from '@navigation';
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+} from '@expo-google-fonts/poppins';
 
 function Main() {
+  let [fontsLoaded] = useFonts({
+    PoppinsRegular: Poppins_400Regular,
+    PoppinsMedium: Poppins_500Medium,
+    PoppinsSemiBold: Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <>
       <Navigation />
---
+++ src/styles/typography.ts
@@ -0,0 +1,27 @@
+export default {
+  title: {
+    fontFamily: 'PoppinsSemiBold',
+    fontSize: 29,
+    lineHeight: 51,
+  },
+  subtitle: {
+    fontFamily: 'PoppinsRegular',
+    fontSize: 28,
+    lineHeight: 42,
+  },
+  text: {
+    fontFamily: 'PoppinsRegular',
+    fontSize: 16,
+    lineHeight: 24,
+  },
+  button: {
+    fontFamily: 'PoppinsMedium',
+    fontSize: 16,
+    lineHeight: 24,
+  },
+  headerTitle: {
+    fontFamily: 'PoppinsMedium',
+    fontSize: 20,
+    lineHeight: 30,
+  },
+};
