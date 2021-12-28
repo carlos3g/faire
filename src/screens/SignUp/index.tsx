ef20bf253d7f2a967b40e47b381d3adc01fce9b9
carlos3g <52337966+carlos3g@users.noreply.github.com>
2021-12-27 21:41:45 -0300

    [FEAT][WIP] Sign Up screen
    To do: Connect fire-base, legibility, types
    
    Made according to: https://www.figma.com/file/jiggpQrS9wyZ0YQ8DCwqft/Faire

--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -1,9 +1,56 @@
-import { Container, Text } from './styles';
+import { useState } from 'react';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
+import { Container, Title, SubTitle, LoginText, Text } from './styles';
+import { Button, Input } from '@components';
+import { RootStackParamList } from '@types';
+
+type SignUpScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'SignUp'
+>;
 
 function SignUp() {
+  const [nome, setNome] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const { navigate } = useNavigation<SignUpScreenNavigationProp>();
+
+  function handleSignUp(): void {
+    console.log(nome);
+    console.log(email);
+    console.log(password);
+  }
+
   return (
     <Container>
-      <Text>Sign Up</Text>
+      <Title>Bem vindo!</Title>
+      <SubTitle>É ótimo te ver aqui.</SubTitle>
+
+      <Input placeholder="Nome" value={nome} onChangeText={setNome} />
+      <Input
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        style={{ marginTop: 10, marginBottom: 10 }}
+      />
+      <Input
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+        style={{ marginBottom: 60 }}
+      />
+
+      <Button
+        label="Cadastrar"
+        onPress={handleSignUp}
+        style={{ marginBottom: 10 }}
+      />
+      <Text>
+        Já tem uma conta?{' '}
+        <LoginText onPress={() => navigate('Login')}>Entre aqui</LoginText>
+      </Text>
     </Container>
   );
 }
--- src/screens/SignUp/styles.ts
+++ src/screens/SignUp/styles.ts
@@ -1,14 +1,44 @@
 import styled from 'styled-components/native';
-import { colors } from '@styles';
+import { KeyboardAvoidingViewProps } from 'react-native';
+import { colors, typography } from '@styles';
+import { shade } from 'polished';
 
-export const Container = styled.View`
+export const Container = styled.KeyboardAvoidingView.attrs(
+  (): KeyboardAvoidingViewProps => ({
+    enabled: true,
+    behavior: 'position',
+    keyboardVerticalOffset: -64,
+  })
+)`
   flex: 1;
   background-color: ${colors.background};
-  justify-content: center;
-  align-items: center;
+  padding: 40px 32px 0 32px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${typography.title.fontFamily};
+  font-size: ${typography.title.fontSize}px;
+  line-height: ${typography.title.lineHeight}px;
+  color: ${colors.text};
+`;
+
+export const SubTitle = styled.Text`
+  font-family: ${typography.subtitle.fontFamily};
+  font-size: ${typography.subtitle.fontSize}px;
+  line-height: ${typography.subtitle.lineHeight}px;
+  color: ${colors.text};
+  margin: 5px 0 40px 0;
 `;
 
 export const Text = styled.Text`
-  font-size: 16px;
-  color: ${colors.text};
+  font-family: ${typography.text.fontFamily};
+  font-size: ${typography.text.fontSize}px;
+  line-height: ${typography.text.lineHeight}px;
+  text-align: center;
+  color: ${shade(0.4, colors.text)};
+  width: 100%;
+`;
+
+export const LoginText = styled.Text`
+  font-family: PoppinsSemiBold;
 `;
